8c14daa9e6066c785512e6681fe3373cf6e371ce
semperubique <lastmurname@gmail.com>
2022-10-06 00:06:11 +0200

    Fix task-list scroll and horizontal overflow for content

--- src/objects.js
+++ src/objects.js
@@ -24,45 +24,4 @@ if (localStorage.getItem("projects")) {
   projects = [];
 }
 
-import { updateUI } from "./updateUI";
-
-const demo = () => {
-  const task1 = Task(
-    "Clean the house",
-    "There are some spiders that are dirty",
-    "2022",
-    10
-  );
-  const task2 = Task(
-    "Remove the mouxxxse",
-    "There are some spiders that are dixrty",
-    "2022",
-    5,
-    "work"
-  );
-  const task3 = Task(
-    "Miyau",
-    "There are some spiders that are dixrty",
-    "2022",
-    5,
-    "work"
-  );
-  const task4 = Task(
-    "All go  the house",
-    "There are some spiders that are dirty",
-    "2022",
-    10
-  );
-  tasks.push(task1);
-  tasks.push(task2);
-  tasks.push(task3);
-  tasks.push(task4);
-  const project1 = Project("work");
-  const project2 = Project("gym");
-  projects.push(project1);
-  projects.push(project2);
-  updateUI.updateProjectList(projects);
-  updateUI.updateTaskList(tasks);
-};
-
-export { Task, Project, demo, tasks, projects };
+export { Task, Project, tasks, projects };
